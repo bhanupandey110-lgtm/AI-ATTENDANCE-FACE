21f3f5cd574c7e20db46adc16f762f0ac36c44f9
Bhanu Pandey <bhanupandey110@gmail.com>
2025-11-28 01:31:00 +0530

    Fixed timezone error

--- Project101/settings.py
+++ Project101/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/kolkata' # add kolkata for india timezone
+TIME_ZONE = 'Asia/Kolkata' # add kolkata for india timezone
 
 USE_I18N = True
 
